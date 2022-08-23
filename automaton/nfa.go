733f6660fcca3d778d2224b2d576a6ceab3a9c22
goropikari <goropikari56@gmail.com>
2022-08-24 08:30:25 +0900

    move def location

--- automaton/nfa.go
+++ automaton/nfa.go
@@ -120,70 +120,6 @@ func (nfa NFA) Star() NFA {
 	return NewNFA(nfa.q, nfa.delta, initStates, initStates)
 }
 
-func (nfa NFA) ToDot() (string, error) {
-	g := graphviz.New()
-	graph, err := g.Graph()
-	if err != nil {
-		return "", err
-	}
-	defer func() {
-		if err := graph.Close(); err != nil {
-			log.Fatal(err)
-		}
-		g.Close()
-	}()
-	graph.SetRankDir("LR") // 図を横長にする
-
-	start, err := graph.CreateNode("start")
-	if err != nil {
-		return "", err
-	}
-	nodes := make(map[State]*cgraph.Node)
-	ii, si, fi := 0, 0, 0
-	for s := range nfa.q {
-		n, err := graph.CreateNode(uuid.New().String()) // assign unique node id
-		if err != nil {
-			return "", err
-		}
-		if nfa.initStates.Contains(s) {
-			e, err := graph.CreateEdge(uuid.New().String(), start, n)
-			if err != nil {
-				return "", err
-			}
-			n.SetLabel(fmt.Sprintf("I%v", ii))
-			e.SetLabel(string(epsilon))
-			ii++
-		}
-		if nfa.finStates.Contains(s) {
-			n.SetShape(cgraph.DoubleCircleShape)
-			n.SetLabel(fmt.Sprintf("F%v", fi))
-			fi++
-		} else {
-			n.SetShape(cgraph.CircleShape)
-			n.SetLabel(fmt.Sprintf("S%v", si))
-			si++
-		}
-		nodes[s] = n
-	}
-
-	for st, qs := range nfa.delta {
-		from := st.First
-		symbol := string(st.Second)
-		for to := range qs {
-			e, err := graph.CreateEdge(charLabel(symbol), nodes[from], nodes[to])
-			if err != nil {
-				return "", err
-			}
-			e.SetLabel(charLabel(symbol))
-		}
-	}
-
-	var buf bytes.Buffer
-	g.Render(graph, "dot", &buf)
-
-	return buf.String(), nil
-}
-
 func (nfa NFA) ToDFA() DFA {
 	que := list.New()
 	memo := collection.NewSet[string]()
@@ -282,4 +218,68 @@ func labelConcat(set collection.Set[State]) string {
 	}
 	slices.Sort(s)
 	return strings.Join(s, "_")
+}
+
+func (nfa NFA) ToDot() (string, error) {
+	g := graphviz.New()
+	graph, err := g.Graph()
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		if err := graph.Close(); err != nil {
+			log.Fatal(err)
+		}
+		g.Close()
+	}()
+	graph.SetRankDir("LR") // 図を横長にする
+
+	start, err := graph.CreateNode("start")
+	if err != nil {
+		return "", err
+	}
+	nodes := make(map[State]*cgraph.Node)
+	ii, si, fi := 0, 0, 0
+	for s := range nfa.q {
+		n, err := graph.CreateNode(uuid.New().String()) // assign unique node id
+		if err != nil {
+			return "", err
+		}
+		if nfa.initStates.Contains(s) {
+			e, err := graph.CreateEdge(uuid.New().String(), start, n)
+			if err != nil {
+				return "", err
+			}
+			n.SetLabel(fmt.Sprintf("I%v", ii))
+			e.SetLabel(string(epsilon))
+			ii++
+		}
+		if nfa.finStates.Contains(s) {
+			n.SetShape(cgraph.DoubleCircleShape)
+			n.SetLabel(fmt.Sprintf("F%v", fi))
+			fi++
+		} else {
+			n.SetShape(cgraph.CircleShape)
+			n.SetLabel(fmt.Sprintf("S%v", si))
+			si++
+		}
+		nodes[s] = n
+	}
+
+	for st, qs := range nfa.delta {
+		from := st.First
+		symbol := string(st.Second)
+		for to := range qs {
+			e, err := graph.CreateEdge(charLabel(symbol), nodes[from], nodes[to])
+			if err != nil {
+				return "", err
+			}
+			e.SetLabel(charLabel(symbol))
+		}
+	}
+
+	var buf bytes.Buffer
+	g.Render(graph, "dot", &buf)
+
+	return buf.String(), nil
 }
